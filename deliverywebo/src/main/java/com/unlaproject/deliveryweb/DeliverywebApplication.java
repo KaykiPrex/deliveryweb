16bcde7586af2b963a61bc888ec1776b1fea49c8
KaykiPrex <jair.garcia.montano@gmail.com>
2021-09-10 18:42:13 -0300

    Release plan - Pedidos sin inicio de sesion

---
+++ deliverywebo/src/main/java/com/unlaproject/deliveryweb/DeliverywebApplication.java
@@ -0,0 +1,13 @@
+package com.unlaproject.deliveryweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverywebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverywebApplication.class, args);
+	}
+
+}
